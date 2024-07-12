bd9bcb2e23de7d5264a730513eae5391783f3257
yogesh-5 <yogeshsoni2555@gmail.com>
2024-07-12 12:47:23 +0530

    first commit

---
+++ JS/index.js
@@ -0,0 +1,58 @@
+
+function myfunction(){
+
+    
+    var x = document.getElementById("first");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+
+
+
+
+}
+
+
+function myfunction1(){
+
+    var y = document.getElementById("second");
+    if (y.style.display === "block") {
+      y.style.display = "none";
+    } else {
+      y.style.display = "block";
+    }
+
+
+}
+
+
+function myfunction2(){
+
+    var z = document.getElementById("third");
+    if (z.style.display === "block") {
+      z.style.display = "none";
+    } else {
+      z.style.display = "block";
+    }
+
+
+}
+
+
+
+
+function myfunction3(){
+
+    var z = document.getElementById("four");
+    if (z.style.display === "block") {
+      z.style.display = "none";
+    } else {
+      z.style.display = "block";
+    }
+
+
+}
+
+
